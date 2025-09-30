8937d9df6c4db360bc0df10349053514e4865194
Kusum Bhattarai Sharma <153010826+kusum-bhattarai@users.noreply.github.com>
2025-09-30 15:14:52 -0500

    Test: Added tests for switching focus between board and nenu

--- tests/test_gameui.cpp
+++ tests/test_gameui.cpp
@@ -6,51 +6,76 @@ class MockGameUI : public GameUI {
 public:
     MockGameUI(SudokuBoard& board) : GameUI(board) {}
 
-    // Helper to directly set the cursor for reliable testing
     void setCursorPosition(int row, int col) {
         cursor_row_ = row;
         cursor_col_ = col;
     }
 
-    // This function will allow us to simulate a key press for testing purposes.
+    void setFocus(FocusState focus) {
+        focus_ = focus;
+    }
+
     bool simulateKeyPress(int ch) {
-        switch (ch) {
-            case 'q': case 'Q': return false;
-            case KEY_RIGHT: {
-                int index = cursor_row_ * SudokuBoard::SIZE + cursor_col_;
-                index = (index + 1) % (SudokuBoard::SIZE * SudokuBoard::SIZE);
-                cursor_row_ = index / SudokuBoard::SIZE;
-                cursor_col_ = index % SudokuBoard::SIZE;
-                break;
-            }
-            case KEY_LEFT: {
-                int index = cursor_row_ * SudokuBoard::SIZE + cursor_col_;
-                index = (index - 1 + (SudokuBoard::SIZE * SudokuBoard::SIZE)) % (SudokuBoard::SIZE * SudokuBoard::SIZE);
-                cursor_row_ = index / SudokuBoard::SIZE;
-                cursor_col_ = index % SudokuBoard::SIZE;
-                break;
-            }
-            case KEY_UP:
-                cursor_row_ = (cursor_row_ - 1 + SudokuBoard::SIZE) % SudokuBoard::SIZE;
-                break;
-            case KEY_DOWN:
-                cursor_row_ = (cursor_row_ + 1) % SudokuBoard::SIZE;
-                break;
-            case '1' ... '9':
-                if (!board_.isPreFilled(cursor_row_, cursor_col_)) {
-                    board_.setCell(cursor_row_, cursor_col_, ch - '0');
+         if (focus_ == FocusState::BOARD) {
+            switch (ch) {
+                case '\t':
+                    focus_ = FocusState::MENU;
+                    break;
+                case 'q': case 'Q': return false;
+                case KEY_RIGHT: {
+                    int index = cursor_row_ * SudokuBoard::SIZE + cursor_col_;
+                    index = (index + 1) % (SudokuBoard::SIZE * SudokuBoard::SIZE);
+                    cursor_row_ = index / SudokuBoard::SIZE;
+                    cursor_col_ = index % SudokuBoard::SIZE;
+                    break;
                 }
-                break;
-            case '0': case KEY_BACKSPACE:
-                if (!board_.isPreFilled(cursor_row_, cursor_col_)) {
-                    board_.setCell(cursor_row_, cursor_col_, 0);
+                case KEY_LEFT: {
+                    int index = cursor_row_ * SudokuBoard::SIZE + cursor_col_;
+                    index = (index - 1 + (SudokuBoard::SIZE * SudokuBoard::SIZE)) % (SudokuBoard::SIZE * SudokuBoard::SIZE);
+                    cursor_row_ = index / SudokuBoard::SIZE;
+                    cursor_col_ = index % SudokuBoard::SIZE;
+                    break;
                 }
+                case KEY_UP:
+                    cursor_row_ = (cursor_row_ - 1 + SudokuBoard::SIZE) % SudokuBoard::SIZE;
+                    break;
+                case KEY_DOWN:
+                    cursor_row_ = (cursor_row_ + 1) % SudokuBoard::SIZE;
+                    break;
+                case '1' ... '9':
+                    if (!board_.isPreFilled(cursor_row_, cursor_col_)) {
+                        board_.setCell(cursor_row_, cursor_col_, ch - '0');
+                    }
+                    break;
+                case '0': case KEY_BACKSPACE:
+                    if (!board_.isPreFilled(cursor_row_, cursor_col_)) {
+                        board_.setCell(cursor_row_, cursor_col_, 0);
+                    }
                 break;
+                default:
+                    break;
+                }
+        } else { // FocusState::MENU
+            switch (ch) {
+                case '\t':
+                    focus_ = FocusState::BOARD;
+                    break;
+                case KEY_UP:
+                    selected_menu_item_ = (selected_menu_item_ - 1 + menu_items_.size()) % menu_items_.size();
+                    break;
+                case KEY_DOWN:
+                    selected_menu_item_ = (selected_menu_item_ + 1) % menu_items_.size();
+                    break;
+                case '\n': case KEY_ENTER:
+                    if (menu_items_[selected_menu_item_] == "Quit") {
+                        return false;
+                    }
+                    break;
+            }
         }
         return true;
     }
 };
-
 
 class GameUITest : public ::testing::Test {
 protected:
@@ -62,11 +87,11 @@ TEST_F(GameUITest, Constructor_InitializesBoardReference) {
     EXPECT_EQ(&ui.getBoard(), &board.getBoard());
 }
 
-TEST_F(GameUITest, Constructor_InitializesCursorAtZero) {
-    auto [row, col] = ui.getCursorPosition();
-    EXPECT_EQ(row, 0);
-    EXPECT_EQ(col, 0);
+TEST_F(GameUITest, Constructor_InitializesUIState) {
+    EXPECT_EQ(ui.getFocus(), GameUI::FocusState::BOARD);
+    EXPECT_EQ(ui.getSelectedMenuItem(), 0);
 }
+
 TEST_F(GameUITest, HandleInput_CursorWrapsAroundBoard) {
     auto [row, col] = ui.getCursorPosition();
 
@@ -133,7 +158,29 @@ TEST_F(GameUITest, HandleInput_ReturnsTrueOnOtherKeys) {
     EXPECT_TRUE(ui.simulateKeyPress('5'));
 }
 
-TEST_F(GameUITest, Constructor_InitializesUIState) {
-    EXPECT_EQ(ui.getFocus(), GameUI::FocusState::BOARD) << "Focus should initially be on the board";
-    EXPECT_EQ(ui.getSelectedMenuItem(), 0) << "Selected menu item should initially be 0";
+TEST_F(GameUITest, HandleInput_SwitchesFocusWithTab) {
+    ASSERT_EQ(ui.getFocus(), GameUI::FocusState::BOARD);
+    
+    ui.simulateKeyPress('\t');
+    EXPECT_EQ(ui.getFocus(), GameUI::FocusState::MENU);
+
+    ui.simulateKeyPress('\t');
+    EXPECT_EQ(ui.getFocus(), GameUI::FocusState::BOARD);
+}
+
+TEST_F(GameUITest, HandleInput_MenuNavigation) {
+    ui.setFocus(GameUI::FocusState::MENU);
+    ASSERT_EQ(ui.getSelectedMenuItem(), 0);
+
+    ui.simulateKeyPress(KEY_DOWN);
+    EXPECT_EQ(ui.getSelectedMenuItem(), 1);
+
+    ui.simulateKeyPress(KEY_UP);
+    EXPECT_EQ(ui.getSelectedMenuItem(), 0);
+
+    ui.simulateKeyPress(KEY_UP);
+    EXPECT_EQ(ui.getSelectedMenuItem(), 4);
+    
+    ui.simulateKeyPress(KEY_DOWN);
+    EXPECT_EQ(ui.getSelectedMenuItem(), 0);
 }
