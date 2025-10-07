a4644b4ffcb26aefb67706e05b1e3d427736b515
Kusum Bhattarai Sharma <153010826+kusum-bhattarai@users.noreply.github.com>
2025-10-07 14:02:33 -0500

    Test: Added gamecontroller tests and updated CMakeLists

---
+++ tests/test_gamecontroller.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+#include "GameController.hpp"
+#include "SudokuBoard.hpp"
+
+// We make the test fixture a friend of the GameController to access its private members
+class GameControllerTest : public ::testing::Test {
+protected:
+    friend class GameController;
+    GameController controller;
+};
+
+TEST_F(GameControllerTest, SubmitIncompleteBoard) {
+    ASSERT_FALSE(controller.board_.isFull());
+    
+    controller.ui_.setFocus(GameUI::FocusState::MENU);
+    controller.ui_.setSelectedMenuItem(0); // 0 is "Submit"
+    controller.processInput('\n');
+
+    EXPECT_TRUE(controller.isRunning());
+    // Verify that the correct message was passed to the UI
+    EXPECT_EQ(controller.ui_.getLastMessage(), "Board is not full yet!");
+}
+
+TEST_F(GameControllerTest, SubmitIncorrectBoard) {
+    // Fill the board, but make it incorrect
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            controller.board_.setCell(i, j, 1); // Fill every cell with '1'
+        }
+    }
+    ASSERT_TRUE(controller.board_.isFull());
+    ASSERT_FALSE(controller.board_.isValid());
+
+    // Simulate selecting "Submit" and pressing Enter
+    controller.ui_.setFocus(GameUI::FocusState::MENU);
+    controller.ui_.setSelectedMenuItem(0);
+    controller.processInput('\n');
+
+    // The game should still be running
+    EXPECT_TRUE(controller.isRunning());
+}
+
+TEST_F(GameControllerTest, SubmitCorrectBoard) {
+    // Generate a full, solved board
+    std::random_device rd;
+    std::mt19937 g(rd());
+    controller.board_.solveBoard(g);
+    ASSERT_TRUE(controller.board_.isFull());
+    ASSERT_TRUE(controller.board_.isValid());
+
+    // Simulate selecting "Submit" and pressing Enter
+    controller.ui_.setFocus(GameUI::FocusState::MENU);
+    controller.ui_.setSelectedMenuItem(0);
+    controller.processInput('\n');
+
+    // The game should stop running
+    EXPECT_FALSE(controller.isRunning());
+}
